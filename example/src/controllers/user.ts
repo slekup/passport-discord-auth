83742cea0df2e88f24b37b78222c7f94bc260c54
slekup <74949101+slekup@users.noreply.github.com>
2023-11-22 17:57:56 +1300

    feat(example): add real route and controller for auth and user

--- example/src/controllers/user.ts
+++ example/src/controllers/user.ts
@@ -1,16 +1,52 @@
 import { Endpoint } from 'express-custom';
 
-export const loginEndpoint = new Endpoint({
-  name: 'Login',
-  description: 'Login to your account',
-  path: '/login',
+import { User } from '@models/User';
+
+export const getUser = new Endpoint({
+  name: 'Get User',
+  description: 'Gets the user',
+  path: '/',
   method: 'GET',
-  /**
-   * Handler for the endpoint.
-   * @param req The request.
-   * @param res The response.
-   */
-  controller: (req, res) => {
-    res.json({ user: 'ok' });
-  },
+}).setController<{ user: User }>((req, res) => {
+  const { profile } = req.user;
+
+  const user = {
+    id: profile.id,
+    username: profile.username,
+    avatar: profile.avatar,
+    global_name: profile.global_name,
+  };
+
+  res.send({
+    user,
+  });
+});
+
+export const getUserProfile = new Endpoint({
+  name: 'Get User Profile',
+  description: 'Gets the user profile',
+  path: '/profile',
+  method: 'GET',
+}).setController<{ user: User }>((req, res) => {
+  const { profile } = req.user;
+
+  const userProfile = {
+    id: profile.id,
+    username: profile.username,
+    avatar: profile.avatar,
+    global_name: profile.global_name,
+    public_flags: profile.public_flags,
+    flags: profile.flags,
+    banner: profile.banner,
+    locale: profile.locale,
+    premium_type: profile.premium_type,
+    guilds: {
+      total: profile.guilds?.length ?? 0,
+      owner: profile.guilds?.filter((guild) => guild.owner).length ?? 0,
+    },
+  };
+
+  res.send({
+    user: userProfile,
+  });
 });
